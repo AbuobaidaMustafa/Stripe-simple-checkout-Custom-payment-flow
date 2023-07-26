3f2cc1880a7df0100b50794cde638dc77ea0d898
unknown <abobeada@gmail.com>
2023-07-26 16:38:57 +0400

    first commit

---
+++ secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51LSa0DDCnqzWwoO0aSL8JW8trkN7AcnsmH4lFNLC1l9ZTXCW62s0LUOogFxJWCj3fHHFWQHm6WQWTTNTWPAq1oRn00Ef8dQOhk';
